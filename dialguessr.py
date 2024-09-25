f02e6991c65ac77e9982634a5943557078721976
louis <louisrethfeld@gmail.com>
2024-09-25 13:52:06 +0200

    adds functionality to change the number of called out digits with argparse. also implements some score metrics.

--- dialguessr.py
+++ dialguessr.py
@@ -1,6 +1,7 @@
 import pyaudio
 import numpy as np
 from time import sleep
+import argparse
 
 
 def generate_samples(frequency: float, duration: float, sampling_rate: int = 44100):
@@ -8,6 +9,11 @@ def generate_samples(frequency: float, duration: float, sampling_rate: int = 441
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Generate a random sequence of DTMF tones and ask the user to guess them.')
+    parser.add_argument('-n', '--number_of_digits', type=int, default=1, help='Number of digits to generate.')
+    args = parser.parse_args()
+
     sound_duration_s = .7
     pause_duration_s = 1
     volume = .5
@@ -17,8 +23,12 @@ if __name__ == '__main__':
     dial_pad = np.array(["1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "0", "#"])
     dp_frequencies = {str(p): (columns[i % 3], rows[i // 3]) for i, p in enumerate(dial_pad)}
     finished = False
+    streak = 0
+    tries = 0
+    wins = 0
     while not finished:
-        random_four_digits = np.random.randint(0, 10, 4)
+
+        random_four_digits = np.random.randint(0, 10, args.number_of_digits)
         frequencies = (dp_frequencies[str(digit)] for digit in random_four_digits)
         sampled_sounds = [generate_samples(frequency[0], sound_duration_s) +
                           generate_samples(frequency[1], sound_duration_s) for frequency in frequencies]
@@ -39,6 +49,15 @@ if __name__ == '__main__':
         if dial_guess == "exit":
             finished = True
         elif dial_guess == "".join(random_four_digits.astype(str)):
-            print("You got it!")
+            streak += 1
+            tries += 1
+            wins += 1
+            status_string = f"{streak=} {wins}/{tries} {wins/tries:.2%}"
+            win_message = f"You got it!"
+            print(f"{win_message:<20} {status_string}")
         else:
-            print("Nope", "".join(random_four_digits.astype(str)))
+            streak = 0
+            tries += 1
+            status_string = f"{streak=} {wins}/{tries} {wins / tries:.2%}"
+            fail_message = f"Nope, it was {"".join(random_four_digits.astype(str))}"
+            print(f"{fail_message:<20} {status_string}")
